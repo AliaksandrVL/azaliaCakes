41872d58867d8837327b3b128fa62b89971300f8
AliaksandrVL <litvinuk2@gmail.com>
2019-05-28 17:09:42 +0300

    product ForeignKey at the images_app model was renamed to owner ForeignKey, as I want to add several dependencies (besides product there must be User)

--- server/images_app/admin.py
+++ server/images_app/admin.py
@@ -5,6 +5,6 @@ from . import models
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
 
-    list_display = ('product', 'image', 'name', 'description')
-    list_filter = ('product', 'name')
-    search_fields = ['product', 'name']
+    list_display = ('owner', 'image', 'name', 'description')
+    list_filter = ('owner', 'name')
+    search_fields = ['owner', 'name']
--- server/images_app/models.py
+++ server/images_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Image(models.Model):
 
-    product = models.ForeignKey('products_app.Product', on_delete=models.CASCADE, verbose_name='Продукт')
+    owner = models.ForeignKey('products_app.Product', on_delete=models.CASCADE, verbose_name='Владелец')
     image = models.ImageField(verbose_name='Изображение')
     name = models.CharField(verbose_name='Наименование', max_length=64, unique=True)
     description = models.TextField(verbose_name='Описание', blank=True)
@@ -11,7 +11,7 @@ class Image(models.Model):
 
     class Meta:
 
-        ordering = ['-product', '-name', '-created', '-modified']
+        ordering = ['-owner', '-name', '-created', '-modified']
 
     def __str__(self):
 
