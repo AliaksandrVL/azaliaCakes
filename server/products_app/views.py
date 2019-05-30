9cb831e6d1617921a90054d9f5db05c9179dd08a
AliaksandrVL <litvinuk2@gmail.com>
2019-05-30 21:05:29 +0300

    Added product_object view for one product

--- server/products_app/views.py
+++ server/products_app/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 from django.http import HttpResponse
 from . import models
 
-def index(request):
+def product_list(request):
 
-    query = models.Product.objects.all()
+    query = get_list_or_404(models.Product)
 
-    return render(request, 'products_app/index.html', {'query': query})
+    return render(request, 'products_app/index.htm', {'query': query})
+
+def product_object(request, pk):
+
+    instance = get_object_or_404(models.Product, id=pk)
+
+    return render(request, 'products_app/object.htm', {'instance': instance})
