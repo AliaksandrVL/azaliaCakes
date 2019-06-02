656eb3a1354da0650306ee94ad6d88a2add6903b
Aliaksandr <litvinuk2@gmail.com>
2019-06-02 18:21:00 +0300

    New Views: update, delete

--- server/products_app/urls.py
+++ server/products_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+
 from . import  views
+
 
 app_name = 'products_app'
 
 urlpatterns=[
-    path('', views.product_list, name='list'),
-    path('create', views.product_create, name='create'),
-    path('<int:pk>/', views.product_object, name='object')
+    path('', views.ProductListView.as_view(), name='list'),
+    path('create', views.ProductCreateView.as_view(), name='create'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='detail'),
+    path('edit/<int:pk>/', views.ProductUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>/', views.ProductDeleteView.as_view(), name='delete')
 ]
