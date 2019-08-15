ad25183543e15d49f514dabd6ea894c2608322b5
Aliaksandr <litvinuk2@gmail.com>
2019-08-15 06:29:02 +0300

    Added class for signin view

--- server/security_app/views.py
+++ server/security_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import FormView
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
 
 from . import forms
 
@@ -19,6 +19,26 @@ class LoginView(FormView):
         if form.is_valid():
             login(request, form.user)
             
+            return redirect(self.success_url)
+        
+        return render(request, self.template_name, {'form': form})
+
+class SigninView(FormView):
+
+    form_class = forms.SigninForm
+    success_url = reverse_lazy('products_app:list')
+    template_name = 'security/signin.htm'
+    main_url = reverse_lazy('products_app:list')
+    
+    def post(self, request):
+
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            
+            login(request, user)
+
             return redirect(self.success_url)
 
         return render(request, self.template_name, {'form': form})
