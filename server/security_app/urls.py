c95ce804fca65aef499a5d118c1a807c0eecb836
Aliaksandr <litvinuk2@gmail.com>
2019-08-15 06:28:29 +0300

    Added path for new signin view

--- server/security_app/urls.py
+++ server/security_app/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'security_app'
 
 urlpatterns = [
+    path('signin/', views.SigninView.as_view(), name='sign'),
     path('', views.LoginView.as_view(), name='login'),
 ]
